8ccc77637ca39ad417b2de8e240adb464c52f660
Mohammed <mrcloud.med@gmail.com>
2024-01-04 21:47:55 +0000

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
-import add from add_0
-a = 1
-b = 2
-print(f"{a} + {b} = {add_0.add(a,b)}")
+from add_0 import add
+if __name__ == "__main__"
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a,b)}", end="\n")
