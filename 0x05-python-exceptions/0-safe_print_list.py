a246500ca33c42eb5b584bacb1c3576c6167093b
Mohammed <mrcloud.med@gmail.com>
2024-01-22 22:04:30 +0000

    update_safe_print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,5 +4,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end=" ")
         print()
+        return x
     except IndexError:
-        pass
+        print("nb_print: {:d}".format(len(my_list)))
+        return len(my_list)
