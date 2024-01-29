5f9c9ab3463cdb3211381ace9b430bb32af33f7f
Mohammed <mrcloud.med@gmail.com>
2024-01-29 10:12:52 -0800

    a + b

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define function add_integer"""
+def add_integer(a, b=98):
+""" function that add 2 integer, and return value"""
+    try:
+        a = int(a)
+    except ValueError:
+        raise TypeError("Error: 'a' must be an integer.")
+    try:
+        b = int(b)
+    except ValueError:
+        raise TypeError("Error: 'b' must be an integer.")
+    return a + b
