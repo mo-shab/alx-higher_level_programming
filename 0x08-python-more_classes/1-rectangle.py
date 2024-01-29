de98d12e15f7a0a5a8108e3ad5376170d99d97e1
Mohammed <mrcloud.med@gmail.com>
2024-01-29 23:13:18 +0100

    1-rect

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle Class"""
+    
+    def __init__(self, width):
+        self._width = width
+    
+    @property
+    def width(self):
+        """Getter method for width"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width"""
+        if not isinstance(value, int):
+            raise TypeError("Width must be an integer")
+        if value < 0:
+            raise ValueError("Width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """Getter method for height"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height"""
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
+        if value < 0:
+            raise ValueError("Height must be >= 0")
+        self._height = value
