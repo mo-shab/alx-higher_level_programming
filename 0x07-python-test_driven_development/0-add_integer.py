537fea7041bca824a218e03f5fc44759cef2a0da
Mohammed <mrcloud.med@gmail.com>
2024-01-29 19:38:37 +0100

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """define function add_integer"""
 def add_integer(a, b=98):
-""" function that add 2 integer, and return value"""
+    """function that add 2 integer
+        and return value"""
     try:
         a = int(a)
     except ValueError:
