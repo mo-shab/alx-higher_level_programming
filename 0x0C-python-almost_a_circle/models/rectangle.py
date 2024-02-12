baa2f8cda83e88c51c5db5f3383a380a33e852d3
Mohammed <mrcloud.med@gmail.com>
2024-02-12 20:27:01 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,8 +7,11 @@ from models.base import Base
 class Rectangle(Base):
     """Define Class Rectangle that inhertance from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''Constructor.'''
         super().__init__(id)
+        self.__width = None
+        self.__height = None
+        self.__x = None
+        self.__y = None
         self.width = width
         self.height = height
         self.x = x
@@ -16,53 +19,55 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''Width of this rectangle.'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.validate_integer("width", value, False)
-        self.__width = value
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        '''Height of this rectangle.'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        self.validate_integer("height", value, False)
-        self.__height = value
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
-        '''x of this rectangle.'''
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.validate_integer("x", value)
-        self.__x = value
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
-        '''y of this rectangle.'''
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.validate_integer("y", value)
-        self.__y = value
-
-    def validate_integer(self, name, value, eq=True):
-        '''Method for validating the value.'''
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if eq and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
-        elif not eq and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
 
     def area(self):
         return self.width * self.height
@@ -94,3 +99,4 @@ class Rectangle(Base):
         elif kwargs and len(kwargs) > 0:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+                
