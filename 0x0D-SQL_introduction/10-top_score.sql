df501e88d0bf820196fd8b7ce46c5014826bb240
Mohammed <mrcloud.med@gmail.com>
2024-03-13 01:55:03 +0100

    TOP SCORE

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Script that lists all record of the table second_table in ordre
-SELECT (score, name) FROM second_table ORDRE BY score DESC;
+SELECT score, name FROM second_table ORDRE BY score DESC;
