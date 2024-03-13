439e07a0fa3db9a0c0b86d9cb65d8bf28bf4a5ed
Mohammed <mrcloud.med@gmail.com>
2024-03-13 01:17:39 +0100

    Task 3 and 4

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- script that lists all table of a database.
+SHOW TABLE;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- script that create a table called first table in the current database
+CREATE TABLE IF NOT EXISTS first_table (id int, name VARCHAR(256));
