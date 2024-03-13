87141be4ab0881b5e8f8a0e59e1d5e6eba52f97e
Mohammed <mrcloud.med@gmail.com>
2024-03-13 02:18:07 +0100

    COUNT(SCORE)

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Script that lists the nuber of records with the same score
-SELECT COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
